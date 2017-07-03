ca0a11e792eb2e8e0d0e42796f00df309b771ba8
freddedotme <r5cx95tw232yvs8w@icloud.com>
2017-07-03 21:37:19 +0200

    Added JSDoc.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 // | Constants
 // -----------------------------------------------------------------------------
 
-const maxClients = 256;
-const maxConversations = 16;
+// const maxClients = 256;
+// const maxConversations = 16;
 const maxMessages = 2048;
 const ws = require('nodejs-websocket');
 
@@ -13,17 +13,74 @@ const MAX_CONVERSATIONS = 'Limit of messages reached. Clearing chat.';
 // | Conversation
 // -----------------------------------------------------------------------------
 
+/** Conversation */
 class Conversation {
+  /**
+   * Create a conversation
+   * @param {number} id Id of the conversation.
+   * @param {number} name Name of the conversation.
+   * @constructor
+   */
   constructor(id, name) {
     this.id = id;
     this.name = name;
     this.messages = [];
   }
 
-  addMessage(message) {
-    if (this.messages.length >= maxMessages) this.messages = [];
-    this.messages.push(MAX_CONVERSATIONS);
-    this.messages.push(message);
+  /**
+   * Add a new message to the conversation, clears chat if limit reached.
+   * @param {number} user User of the message.
+   * @param {string} message Message.
+   */
+  addMessage(user, message) {
+    if (this.messages.length >= maxMessages) {
+      this.messages = [];
+      this.messages.push(
+          new Message(-1, MAX_CONVERSATIONS, getFormattedDate()));
+    }
+    this.messages.push(
+        new Message(user.getId(), message, getFormattedDate()));
+  }
+}
+
+// -----------------------------------------------------------------------------
+// | Message
+// -----------------------------------------------------------------------------
+
+/** Message */
+class Message {
+  /**
+   * Create a message
+   * @param {number} user User of the conversation.
+   * @param {string} message Message.
+   * @param {string} date When the message was sent.
+   * @constructor
+   */
+  constructor(user, message, date) {
+    this.user = user;
+    this.message = message;
+    this.date = date;
+  }
+
+  /**
+   * @return {int} The user id.
+   */
+  getUser() {
+    return this.user;
+  }
+
+  /**
+   * @return {string} The message.
+   */
+  getMessage() {
+    return this.message;
+  }
+
+  /**
+   * @return {string} The date of the message.
+   */
+  getDate() {
+    return this.date;
   }
 }
 
@@ -31,51 +88,75 @@ class Conversation {
 // | User
 // -----------------------------------------------------------------------------
 
+/** User */
 class User {
-  constructor(client, id, name, picture, conversation) {
+  /**
+   * Create a message
+   * @param {object} client Client, used as identifier.
+   * @param {number} id Id of the user.
+   * @param {string} name Name of the user.
+   * @param {number} conversation Id of active conversation.
+   * @constructor
+   */
+  constructor(client, id, name, conversation) {
     this.client = client;
     this.id = id;
     this.name = name;
-    this.picture = picture;
     this.conversation = conversation;
   }
 
+  /**
+   * @return {object} The client.
+   */
   getClient() {
     return this.client;
   }
 
+  /**
+   * @param {object} client Set client.
+   */
   setClient(client) {
     this.client = client;
   }
 
+  /**
+   * @return {number} The user id.
+   */
   getId() {
     return this.id;
   }
 
+  /**
+   * @param {number} id Set id.
+   */
   setId(id) {
     this.id = id;
   }
 
+  /**
+   * @return {string} The name.
+   */
   getName() {
     return this.name;
   }
 
+  /**
+   * @param {string} name Set name.
+   */
   setName(name) {
     this.name = name;
   }
 
-  getPicture() {
-    return this.picture;
-  }
-
-  setPicture(picture) {
-    this.picture = picture;
-  }
-
+  /**
+   * @return {id} The conversation.
+   */
   getConversation() {
     return this.conversation;
   }
 
+  /**
+   * @param {number} conversation Set conversation.
+   */
   setConversation(conversation) {
     this.conversation = conversation;
   }
@@ -93,25 +174,27 @@ conversations.push(new Conversation(0, 'General'));
 conversations.push(new Conversation(1, 'Sports'));
 conversations.push(new Conversation(2, 'Gaming'));
 
-let server = ws.createServer(function(client) {
+ws.createServer(function(client) {
   addUser(client);
 
   client.on('text', function(text) {
-
     let user = getUser(client);
-    if (!user) return;
+    if (user == null) return;
 
     let data = JSON.parse(text);
     let action = data.action;
     let value = data.value;
-    if (!action || !value) return;
+
+    if (action == null || value == null) return;
     action = parseInt(action);
 
     switch (action) {
       case 0:
         // send message
-        let conversation = user.getConversation();
-        conversation.addMessage(value);
+        console.log('runs');
+        let index = user.getConversation();
+        console.log('runs');
+        conversations[index].addMessage(user, value);
         update();
         break;
       case 1:
@@ -120,11 +203,6 @@ let server = ws.createServer(function(client) {
         update();
         break;
       case 2:
-        // set picture
-        user.setPicture(value);
-        update();
-        break;
-      case 3:
         // set conversation
         value = parseInt(value);
         if (value < 0 || value >= conversations.length) return;
@@ -144,17 +222,30 @@ let server = ws.createServer(function(client) {
 // | Communication
 // -----------------------------------------------------------------------------
 
+/**
+ * Sends an update with the newest data.
+ */
 function update() {
   let data = '{"users":' + JSON.stringify(users, filter) + ',' +
       '"conversations":' + JSON.stringify(conversations) + '}';
   broadcast(data);
 }
 
+/**
+ * Filters JSON from specific keys.
+ * @param {string} key The JSON key.
+ * @param {*} value The value of JSON key.
+ * @return {*} value If the key is allowed it returns it.
+ */
 function filter(key, value) {
   if (key === 'client') return undefined;
   return value;
 }
 
+/**
+ * Broadcast data to all users.
+ * @param {string} data Data to be broadcast.
+ */
 function broadcast(data) {
   for (let i = 0; i < users.length; i++) {
     users[i].getClient().send(data);
@@ -165,23 +256,64 @@ function broadcast(data) {
 // | Misc
 // -----------------------------------------------------------------------------
 
+/**
+ * Add user to users list.
+ * @param {object} client Client, used as identifier.
+ */
 function addUser(client) {
-  users.push(new User(client, visits, 'random', 'image.jpg'));
+  users.push(new User(client, visits, 'random', 1));
 }
 
+/**
+ * Remove user from users list.
+ * @param {object} client Client, used as identifier.
+ */
 function removeUser(client) {
   let index = getUserIndex(client);
+  if (index == null) return;
   users.splice(index, 1);
 }
 
+/**
+ * Get a user from users list.
+ * @param {object} client Client, used as identifier.
+ * @return {object} user The user.
+ */
 function getUser(client) {
   let index = getUserIndex(client);
+  if (index == null) return null;
   return users[index];
 }
 
+/**
+ * Get user index in users list.
+ * @param {object} client Client, used as identifier.
+ * @return {number} i The index.
+ * @return {null} null Returns null if user not found.
+ */
 function getUserIndex(client) {
   for (let i = 0; i < users.length; i++) {
     if (users[i].getClient() === client) return i;
   }
   return null;
+}
+
+/**
+ * Get date as a formatted string.
+ * @return {string} date The formatted date as a string.
+ */
+function getFormattedDate() {
+  let today = new Date();
+  let mm = today.getMinutes();
+  let hh = today.getHours();
+  let dd = today.getDate();
+  let MM = today.getMonth() + 1;
+  let yyyy = today.getFullYear();
+
+  if (dd < 10) dd = '0' + dd;
+  if (MM < 10) MM = '0' + MM;
+  if (hh < 10) hh = '0' + hh;
+  if (mm < 10) mm = '0' + mm;
+
+  return yyyy + '-' + MM + '-' + dd + ' ' + hh + ':' + mm;
 }
--- client.js
+++ client.js
@@ -1,16 +1,46 @@
-var socket = new WebSocket('ws://localhost:8001');
+let socket = new WebSocket('ws://localhost:8001');
 
 socket.addEventListener('open', function(event) {
   debug();
 });
 
 socket.addEventListener('message', function(event) {
-  let data = JSON.parse(event.data);
+  const data = JSON.parse(event.data);
   console.log(data);
+
+  let htmlUsers = document.getElementsByClassName('users')[0];
+  let htmlConversations = document.getElementsByClassName('conversations')[0];
+
+  let dataUsers = data.users;
+  let dataConversations = data.conversations;
+
+  htmlUsers.innerHTML = '';
+  htmlConversations = '';
+
+  for (let i = 0; i < dataUsers.length; i++) {
+    let li = document.createElement('li');
+    let id = document.createElement('p');
+    let name = document.createElement('p');
+
+    id.innerHTML = '#' + dataUsers[i].id;
+    name.innerHTML = dataUsers[i].name;
+
+    li.appendChild(id);
+    li.appendChild(name);
+    htmlUsers.appendChild(li);
+  }
+
+  for (let i = 0; i < htmlConversations.length; i++) {
+
+  }
 });
 
+/**
+ * Test case.
+ */
 function debug() {
+  socket.send('{"action":0,"value":"Hey! Are you new?"}');
   socket.send('{"action":1,"value":"Borat"}');
   socket.send(
-      '{"action":2,"value":"https://s-media-cache-ak0.pinimg.com/736x/92/9d/3d/929d3d9f76f406b5ac6020323d2d32dc--pretty-cats-beautiful-cats.jpg"}');
+      '{"action":2,"value":0}');
 }
